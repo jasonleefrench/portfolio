c4a14d0cc49c243962041c08e85232bbc74b7eff
jasonleefrench <jason.french@wsj.com>
2021-08-05 21:50:54 +0100

    oops

--- components/greeter.js
+++ components/greeter.js
@@ -5,6 +5,7 @@ const secondsInDay = 1000 * 60 * 60 * 24
 
 const Greeter = ({ text }) => {
   const [ lastVisit, setLastVisit ] = useLocalStorage('last-visit')
+  console.log(lastVisit)
   const [ greeting, setGreeting ] = useState('')
   useEffect(() => {
     const revisit = lastVisit && secondsInDay > Date.now() - +lastVisit
@@ -15,7 +16,7 @@ const Greeter = ({ text }) => {
       'Good evening!'
     setGreeting(greeting)
     setLastVisit(+Date.now())
-  }, [ lastVisit, setLastVisit ])
+  }, [])
   const editable = text.match(/{(.+)}/)[1]
   const __html = text.split(/{(.+)}/)
     .map((str, index) => index === 1 ? greeting : str)
