ebca95fa5d59f9594f62e0752a6b847aa0ffdda7
jasonleefrench <jason.french@wsj.com>
2021-08-05 21:27:12 +0100

    get rid of weird loading !

--- components/greeter.js
+++ components/greeter.js
@@ -5,14 +5,14 @@ const secondsInDay = 1000 * 60 * 60 * 24
 
 const Greeter = ({ text }) => {
   const [ lastVisit, setLastVisit ] = useLocalStorage('last-visit')
-  const [ greeting, setGreeting ] = useState('Hello')
+  const [ greeting, setGreeting ] = useState('')
   useEffect(() => {
     const revisit = lastVisit && secondsInDay > Date.now() - +lastVisit
     const hours = new Date().getHours()
-    const greeting = revisit ? 'Hi again' :
-      hours < 12 ? 'Good morning' :
-      hours < 18 ? 'Howdy' :
-      'Good evening'
+    const greeting = revisit ? 'Hi again!' :
+      hours < 12 ? 'Good morning!' :
+      hours < 18 ? 'Howdy!' :
+      'Good evening!'
     setGreeting(greeting)
     setLastVisit(+Date.now())
   }, [ lastVisit, setLastVisit ])
