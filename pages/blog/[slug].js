f34522cb9a7c1b8f815fb11b9d625edb74e26011
jasonleefrench <jason.french@wsj.com>
2021-11-04 10:48:20 +0000

    fix errors

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -53,9 +53,9 @@ const serializers = {
           width='100%'
           height='500'
           src={`https://www.youtube.com/embed/${code}`}
-          frameborder='0'
+          frameBorder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       )
     }
