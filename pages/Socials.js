c79e2eeb19a25a8dcb2b5401e98b83e387a730c8
jasonleefrench <jason.french@wsj.com>
2021-12-30 16:36:29 +0000

    don't lazy-load the sprites

--- pages/Socials.js
+++ pages/Socials.js
@@ -2,16 +2,22 @@ import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 
 const Socials = ({ data = [] }) => (
-  <section className={styles.socials}>
-    <h2>Contact me</h2>
-    <div className={styles.holder}>
-      {data.map(({ image, link, alt }, key) => (
-        <a key={key} href={link}>
-          <Image src={image} alt={alt} width={48} height={40} />
-        </a>
-      ))}
-    </div>
-  </section>
+    <section className={styles.socials}>
+        <h2>Contact me</h2>
+        <div className={styles.holder}>
+            {data.map(({ image, link, alt }, key) => (
+                <a key={key} href={link}>
+                    <Image
+                        loading="eager"
+                        src={image}
+                        alt={alt}
+                        width={48}
+                        height={40}
+                    />
+                </a>
+            ))}
+        </div>
+    </section>
 )
 
 export default Socials
