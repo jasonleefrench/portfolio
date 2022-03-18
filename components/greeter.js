de321ccea6aa3aba0155ed882139af7ede30e936
jasonleefrench <jason.french@wsj.com>
2022-03-18 23:21:20 +0000

    refresh

--- components/greeter.js
+++ components/greeter.js
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
+import styles from '../styles/Home.module.css'
 
 const secondsInHalfDay = (1000 * 60 * 60 * 24) / 2
 
 const Greeter = ({ text }) => {
-  const [ lastVisit, setLastVisit ] = useLocalStorage('last-visit')
-  const [ greeting, setGreeting ] = useState('')
-  useEffect(() => {
-    const revisit = lastVisit && (secondsInHalfDay > Date.now() - +lastVisit)
-    const hours = new Date().getHours()
-    const greeting = revisit ? 'Hi again!' :
-      hours < 12 ? 'Good morning!' :
-      hours < 18 ? 'Howdy!' :
-      'Good evening!'
-    setGreeting(greeting)
-    setLastVisit(+Date.now())
-  }, [])
-  const editable = text.match(/{(.+)}/)[1]
-  const __html = text.split(/{(.+)}/)
-    .map((str, index) => index === 1 ? greeting : str)
-    .join('')
-  return <span className='greeter' dangerouslySetInnerHTML={{__html}} />
+    const [lastVisit, setLastVisit] = useLocalStorage('last-visit')
+    const [greeting, setGreeting] = useState('')
+    useEffect(() => {
+        const revisit = lastVisit && secondsInHalfDay > Date.now() - +lastVisit
+        const hours = new Date().getHours()
+        const greeting = revisit
+            ? 'Hi again! 👋'
+            : hours < 12
+            ? 'Good morning! ☀️'
+            : hours < 18
+            ? 'Howdy! 🤠'
+            : 'Good evening! 🌙'
+        setGreeting(greeting)
+        setLastVisit(+Date.now())
+    }, [])
+    const editable = text.match(/{(.+)}/)[1]
+    const __html = text
+        .split(/{(.+)}/)
+        .map((str, index) => (index === 1 ? greeting : str))
+        .join('')
+    return (
+        <span className={styles.greeter} dangerouslySetInnerHTML={{ __html }} />
+    )
 }
 
 export default Greeter
